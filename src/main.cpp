095ab08578d1a08d0325eae1b462f6bab991253f
Sunaa Risu <sunaa@sunaarisu.de>
2025-01-11 22:54:52 +0100

    temp buffer classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,10 @@
 #include <Stitch/stitch.h>
-#include <iostream>
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 #define WIDTH 1080
 #define HEIGHT 1080
-
-#define ASSERT(x) if (!(x)) __builtin_trap();
-#define GLCall(x) GLClearError();x;ASSERT(GLLogCall(#x, __FILE__, __LINE__));
-
-static void GLClearError() {
-  while (glGetError());
-}
-
-static bool GLLogCall(const char* function, const char* file, int line) {
-  while (GLenum error = glGetError()) {
-    std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
-    return false;
-  }
-  return true;
-}
 
 int main() {
   Display display(WIDTH, HEIGHT, "Tag-Game");
@@ -34,18 +21,16 @@ int main() {
     2, 3, 0
   };
 
-  unsigned int buffer;
-  glGenBuffers(1, &buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, buffer);
-  glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
+  unsigned int vertexArrayObject;
+  GLCall(glGenVertexArrays(1, &vertexArrayObject));
+  GLCall(glBindVertexArray(vertexArrayObject));
+
+  VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
   glEnableVertexAttribArray(0);
 
-  unsigned int indexBufferObject;
-  glGenBuffers(1, &indexBufferObject);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+  IndexBuffer ib(indices, 6);
 
   Shader basicShader("res/shaders/basic.shader");
   basicShader.Bind();
@@ -61,6 +46,10 @@ int main() {
     display.Clear(0.1f, 0.1f, 0.1f, 1.0f);
     
     GLCall(glUniform4f(location, r, 0.0f, 1.0f, 1.0f));
+
+    GLCall(glBindVertexArray(vertexArrayObject));
+    ib.Bind(); 
+
     GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
     if (r > 1.0f)
